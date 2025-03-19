ed03f411d767377f628d7118da2357c4e69b4661
Abigail Bentley <sgabentl@liverpool.ac.uk>
2025-03-19 15:52:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from wed12 import ColourDetection
+from colour_detection import ColourDetection
 from test_position import PositionMove
+# from stirrer import IKADriver
+import time
 
 arm = PositionMove()
+arm.robot_conn()
 arm.move_to_stirrer()
+
+# stirrer = IKADriver(serial_port="/dev/ttyACM0")
+# stirrer.setStir(1000)
+# stirrer.startStir()
 
 colour_detection = ColourDetection()
 colour_detection.capture()
 colour_detection.plot_graph()
+
+# stirrer.stopStir()
+arm.remove_from_stirrer()
