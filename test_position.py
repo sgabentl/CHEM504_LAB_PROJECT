08ebde14fa5101a8661353ca810652b3ec67a619
Abigail Bentley <sgabentl@liverpool.ac.uk>
2025-03-26 13:03:35 +0000

    Update test_position.py

--- test_position.py
+++ test_position.py
@@ -22,16 +22,25 @@ class PositionHandler:
           self.robot.move_joint_list(position, 0.25, 0.5, 0.02)
 
      
-class PositionMove:         
-     RED_POSITION1 = [1.7446362972259521, -1.4385647040656586, 2.3446999231921595, -0.7653154295733948, 1.5420920848846436, 3.153193950653076]
-     RED_POSITION2 = [1.730034589767456, -1.0733220440200348, 2.283058468495504, -1.201580212717392, 1.542656660079956, 3.153193950653076]
-     GREEN_POSITION1 = [1.7241122722625732, -1.6038876972594203, 2.0562947432147425, -0.45576222360644536, 1.5195368528366089, 3.1572272777557373]
-     GREEN_POSITION2 = [1.1704504489898682, -2.112025877038473, 2.47654635110964, -0.3685503763011475, 1.168399691581726, 3.1618423461914062]
-     GREEN_POSITION3 = [1.169089436531067, -1.8085299930968226, 2.695914093648092, -0.8905757826617737, 1.1692121028900146, 3.1597652435302734]
-     #GREEN_POSITION2 = [1.174616813659668, -2.16580929378652, 2.3327489534961146, -0.15603549898181157, 1.1721503734588623, 3.1608855724334717]
-     #GREEN_POSITION3 = [1.1731359958648682, -1.8137570820250453, 2.6901567617999476, -0.8645797532847901, 1.1737935543060303, 3.1583807468414307]
-     RED_POSITION3 = [1.7239357233047485, -1.5127788682333012, 2.154865090047018, -0.6454361242106934, 1.5201416015625, 3.1564512252807617]
-     RED_POSITION4 = [1.7227802276611328, -1.0755092662623902, 2.2902348677264612, -1.2178798180869599, 1.5208289623260498, 3.153461456298828]
+class PositionMove:   
+     # Automated stirrer might be a different height      
+     STIRRER_1 = [1.7241122722625732, -1.6038876972594203, 2.0562947432147425, -0.45576222360644536, 1.5195368528366089, 3.1572272777557373]
+     STIRRER_2 = [1.1704504489898682, -2.112025877038473, 2.47654635110964, -0.3685503763011475, 1.168399691581726, 3.1618423461914062]
+     STIRRER_3 = [1.169089436531067, -1.8085299930968226, 2.695914093648092, -0.8905757826617737, 1.1692121028900146, 3.1597652435302734]
+     
+     LOAD_1 = [1.7239357233047485, -1.5127788682333012, 2.154865090047018, -0.6454361242106934, 1.5201416015625, 3.1564512252807617]
+     LOAD_2 = [1.7227802276611328, -1.0755092662623902, 2.2902348677264612, -1.2178798180869599, 1.5208289623260498, 3.153461456298828]
+     LOAD_3 = [1.7811172008514404, -1.0510652226260682, 2.182856861745016, -1.1096548003009339, 1.6842586994171143, 3.182462215423584]
+     LOAD_4 = [1.7821320295333862, -1.4705685314587136, 2.0160797278033655, -0.5230320256999512, 1.6831088066101074, 3.185417652130127]
+     
+     UNLOAD_1 = [1.1704657077789307, -2.1179315052428187, 2.4668949286090296, -0.35293133676562505, 1.168339729309082, 3.1618471145629883]
+     UNLOAD_2 = [0.7989100813865662, -1.6750875912108363, 2.1542938391314905, -0.3579598230174561, 1.0720365047454834, 3.1202919483184814]
+     UNLOAD_3 = [0.79803067445755, -1.3141046327403565, 2.3614562193499964, -0.9247894448092957, 1.073585867881775, 3.1175761222839355]
+     UNLOAD_4 = [0.7989722490310669, -1.2075361174396058, 2.3517192045794886, -1.0154329103282471, 1.063684105873108, 3.117006778717041]
+     UNLOAD_5 = [0.8142712116241455, -1.6682263813414515, 2.2509000937091272, -0.5373445314219971, 0.9473036527633667, 3.1304078102111816]
+     UNLOAD_6 = [0.813372790813446, -1.3423428249410172, 2.407254759465353, -1.0180101555636902, 0.9482195377349854, 3.1276650428771973]
+     UNLOAD_7 = [0.8128491640090942, -1.1640876692584534, 2.428725067769186, -1.2169773441604157, 0.9481956958770752, 3.1262495517730713]
+     
      
      def __init__(self):
           self.gripper = Gripper()
@@ -41,23 +50,45 @@ class PositionMove:
           self.robot = URControl(ip="192.168.0.2", port=30003)
           self.positionHandler = PositionHandler(self.robot)
 
-     def move_to_stirrer(self):
-          self.positionHandler.move_to_position(self.RED_POSITION3)
-          self.positionHandler.move_to_position(self.RED_POSITION4)
+     def load_sample_1(self):
+          self.positionHandler.move_to_position(self.LOAD_1)
+          self.positionHandler.move_to_position(self.LOAD_2)
           self.gripper.close_grip()
-          self.positionHandler.move_to_position(self.RED_POSITION3)
-          self.positionHandler.move_to_position(self.GREEN_POSITION1)
-          self.positionHandler.move_to_position(self.GREEN_POSITION2)
-          self.positionHandler.move_to_position(self.GREEN_POSITION3)
+          self.positionHandler.move_to_position(self.LOAD_1)
+          self.positionHandler.move_to_position(self.STIRRER_1)
+          self.positionHandler.move_to_position(self.STIRRER_2)
+          self.positionHandler.move_to_position(self.STIRRER_3)
           self.gripper.open_grip()
-          self.positionHandler.move_to_position(self.GREEN_POSITION2)
+          self.positionHandler.move_to_position(self.STIRRER_2)
           
-     def remove_from_stirrer(self):
-          self.positionHandler.move_to_position(self.GREEN_POSITION3)
+     def unload_sample_1(self):
+          self.positionHandler.move_to_position(self.STIRRER_3)
           self.gripper.close_grip()
-          self.positionHandler.move_to_position(self.GREEN_POSITION2)
-          self.positionHandler.move_to_position(self.GREEN_POSITION1)
-          self.positionHandler.move_to_position(self.RED_POSITION3)
-          self.positionHandler.move_to_position(self.RED_POSITION4)
+          self.positionHandler.move_to_position(self.UNLOAD_1)
+          self.positionHandler.move_to_position(self.UNLOAD_2)
+          self.positionHandler.move_to_position(self.UNLOAD_3)
+          self.positionHandler.move_to_position(self.UNLOAD_4)
           self.gripper.open_grip()
-          self.positionHandler.move_to_position(self.RED_POSITION1)
+          self.positionHandler.move_to_position(self.UNLOAD_2)
+
+     
+     def load_sample_2(self):
+          self.positionHandler.move_to_position(self.LOAD_1)
+          self.positionHandler.move_to_position(self.LOAD_3)
+          self.gripper.close_grip()
+          self.positionHandler.move_to_position(self.LOAD_4)
+          self.positionHandler.move_to_position(self.STIRRER_2)
+          self.positionHandler.move_to_position(self.STIRRER_3)
+          self.gripper.open_grip()
+          self.positionHandler.move_to_position(self.STIRRER_2)
+          
+     def unload_sample_2(self):
+          self.positionHandler.move_to_position(self.STIRRER_3)
+          self.gripper.close_grip()
+          self.positionHandler.move_to_position(self.UNLOAD_1)
+          self.positionHandler.move_to_position(self.UNLOAD_5)
+          self.positionHandler.move_to_position(self.UNLOAD_6)
+          self.positionHandler.move_to_position(self.UNLOAD_7)
+          self.gripper.open_grip()
+          self.positionHandler.move_to_position(self.UNLOAD_5)
+          
