3e6b009cf229a81c061b5d61b91db87880c018f7
Abigail Bentley <sgabentl@liverpool.ac.uk>
2025-03-26 13:02:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from colour_detection import ColourDetection
+from colour_detect_red import ColourDetection as REDColour
+from colour_detection import ColourDetection as BLUEColour
 from test_position import PositionMove
 # from stirrer import IKADriver
-import time
+from graph import Graph
 
 arm = PositionMove()
 arm.robot_conn()
-arm.move_to_stirrer()
+arm.load_sample_1()
 
-# stirrer = IKADriver(serial_port="/dev/ttyACM0")
+# Next week ask what the always allow chmod 666 code is
+# stirrer = IKADriver(serial_port="")
 # stirrer.setStir(1000)
 # stirrer.startStir()
 
-colour_detection = ColourDetection()
-colour_detection.capture()
-colour_detection.plot_graph()
+# colour_detection_red = REDColour()
+# colour_detection_red.capture()
+# colour_detection_red.plot_graph()
+
+# colour_detection = BLUEColour()
+# colour_detection.capture()
+# colour_detection.plot_graph()
+
+graph = Graph()
+graph.get_rgb()
 
 # stirrer.stopStir()
-arm.remove_from_stirrer()
+arm.unload_sample_1()
+arm.load_sample_2()
+
+graph.get_rgb()
+
+arm.unload_sample_2()
