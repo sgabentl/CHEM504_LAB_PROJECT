9896e418dc8a8fc66030ba2b87e77d8e95ad6506
Abigail Bentley <sgabentl@liverpool.ac.uk>
2025-04-02 12:12:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
-from colour_detect_red import ColourDetection as REDColour
-from colour_detection import ColourDetection as BLUEColour
 from test_position import PositionMove
-# from stirrer import IKADriver
+from stirrer import IKADriver
 from graph import Graph
 
+PORT = "serial_port"
 def run_program():
     """Loads 2 samples to stirrer plate and """
     arm = PositionMove()
     arm.robot_conn()
     arm.load_sample_1()
 
-    # Next week ask what the always allow chmod 666 code is
-    # Unlock with sumo python3 file_path
-    # stirrer = IKADriver(serial_port="/dev/ttyACM0")
-    # stirrer.setStir(1000)
-    # stirrer.startStir()
+    # Add a sleep for the graph destroy window
+    graph1 = Graph()
+    graph1.get_rgb()
 
-    # colour_detection_red = REDColour()
-    # colour_detection_red.capture()
-    # colour_detection_red.plot_graph()
+    arm.unload_sample_1()
+    arm.load_sample_2()
 
-    # colour_detection = BLUEColour()
-    # colour_detection.capture()
-    # colour_detection.plot_graph()
+    graph2 = Graph()
+    graph2.get_rgb()
+
+    arm.unload_sample_2()
+
+def run_program_with_auto_stirrer():
+    """Loads 2 samples to stirrer plate and """
+    arm = PositionMove()
+    arm.robot_conn()
+    arm.load_sample_1()
+
+    stirrer = IKADriver(serial_port=PORT)
+    stirrer.setStir(1000)
+    stirrer.startStir()
 
     # Add a sleep for the graph destroy window
     graph1 = Graph()
     graph1.get_rgb()
 
-    # stirrer.stopStir()
+    stirrer.stopStir()
     arm.unload_sample_1()
     arm.load_sample_2()
-    # stirrer.setStir(1000)
-    # stirrer.startStir()
+    stirrer.setStir(1000)
+    stirrer.startStir()
 
     graph2 = Graph()
     graph2.get_rgb()
-    # stirrer.stopStir()
+    stirrer.stopStir()
 
     arm.unload_sample_2()
 
-
 run_program()
+
