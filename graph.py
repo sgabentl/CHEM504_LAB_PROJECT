19a8a099925f4f1446def53fd2694b4ae20ba973
Isabel Dixon <sgidixon@liverpool.ac.uk>
2025-05-07 07:49:03 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -87,8 +87,13 @@ class Graph:
             self.rgb_dict["time"].append(elapsed_time)
 
             # Create window with colour status and ROI
-            colour_status = "Blue (Oxidized)" if self.rgb_dict["red"] < 50 else "Transitioning"
-            colour_status = "Red" if self.rgb["blue"] < 50 else "Transitioning"
+            if self.rgb_dict["red"] < 50:
+                colour_status = "Blue"
+            elif self.rgb_dict["blue"] < 50:
+                colour_status = "Red"
+            else:
+                colour_status = "Transitioning
+
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, f"Status: {colour_status}", (20, 50),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
